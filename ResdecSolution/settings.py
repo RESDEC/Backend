a630cc517c588041185a3699ecd7b293775f4bc4
Rostan <allowed_g@hotmail.com>
2018-07-01 17:16:21 -0500

    Commit #10
    Cambios en los settings por la ruta de los archivos estaticos.

--- ResdecSolution/settings.py
+++ ResdecSolution/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
-STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "staticfiles")
+STATIC_ROOT = os.path.join(os.path.dirname(BASE_DIR), "static")
 STATIC_URL = '/static/'
 
 # Extra places for collecstatic to find static files
